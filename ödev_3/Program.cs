2acf7c6af47429eb9798738b0ae2382b9fa114e5
Hp <Hp@DESKTOP-LTJOJD3>
2023-05-27 21:34:37 +0300

    Add project files.

---
+++ ödev_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ödev_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            for (int a = 1; a <= 10; a++)
+            {
+                if (a % 2 != 0)
+                {
+                    toplam += a;
+                }
+            }
+            Console.WriteLine(toplam);
+            Console.ReadKey();
+        }
+    }
+}
